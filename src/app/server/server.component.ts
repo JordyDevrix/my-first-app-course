5f9f967885ccea5184acd276ec5229fc9bfd4037
Jordy Devrix <2109jordy2241@gmail.com>
2024-03-12 23:08:12 +0100

    binding and interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html'
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit{
+    serverId = 10;
+    serverStatus = 'offline';
+    allowNewServer = true;
+
+    constructor() {
+        setTimeout(() => {
+            this.allowNewServer = false;
+        }, 2000);
+    }
+
+    ngOnInit() {
+        
+    }
 
 }
