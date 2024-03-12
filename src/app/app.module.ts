d54d4858a3ba03f1da8c6ce4cbb3be36cbf6d983
Jordy Devrix <2109jordy2241@gmail.com>
2024-03-12 22:29:10 +0100

    followed some lessons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlert } from './warningalert/warningalert.component';
+import { SuccesalertComponent } from './succesalert/succesalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    WarningAlert,
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccesalertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
